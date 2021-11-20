db8d01614c3bec583a28368de5cd241a944f8ec9
Robyn <robynthecryptid66@gmail.com>
2021-11-20 16:12:59 +1100

    0.0.0.2
    - Added population
    - Added randomly generated seed population consisting of 100 organisms

--- main.py
+++ main.py
@@ -27,4 +27,18 @@ templateOrganism = {"MR": [1, 3],
                     "GP": [2, 4],
                     "F": [1, 50],
                     "MaxTemp": [5, 15],
-                    "MinTemp": [(0-5), (0-15)]}
+                    "MinTemp": [(5), (15)]}
+
+print("Generating population...")
+population = []
+while len(population) < 100:
+    toad1 = {"MR": random.randint(templateOrganism["MR"][0], templateOrganism["MR"][1]),
+                        "MS": random.randint(templateOrganism["MS"][0], templateOrganism["MS"][1]),
+                        "L": random.randint(templateOrganism["L"][0], templateOrganism["L"][1]),
+                        "LS": random.randint(templateOrganism["LS"][0], templateOrganism["LS"][1]),
+                        "GP": random.randint(templateOrganism["GP"][0], templateOrganism["GP"][1]),
+                        "F": random.randint(templateOrganism["F"][0], templateOrganism["F"][1]),
+                        "MaxTemp": random.randint(templateOrganism["MaxTemp"][0], templateOrganism["MaxTemp"][1]),
+                        "MinTemp": 0-random.randint(templateOrganism["MinTemp"][0], templateOrganism["MinTemp"][1])}
+    population.append(toad1)
+    print(f"Organism {len(population)} generated")
